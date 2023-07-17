a62007dc62308f7c2a8ff39d37513c256f398aa4
jhl221123 <jhl221123@naver.com>
2023-07-17 23:25:21 +0900

    쉘 스크립트 실행 테스트

--- post-message.sh
+++ post-message.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 message="[TEST]TIL 작성했습니다."
+SLACK_WEBHOOK_URL=$1
 
 curl -H "Content-type: application/json; charset=utf-8" \
     --data '{"author_name": "이준혁", "attachments": [{"text": '"\"${message}\""'}]}' \
-    -X POST ${secrets.SLACK_WEBHOOK_URL}
+    -X POST ${SLACK_WEBHOOK_URL}
